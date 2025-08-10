dd0c7c47eaff28b407cab8160caa052971979ec2
tanishq <bawatanishq3105@gmail.com>
2025-08-10 09:36:06 +0000

    Task 3: Terraform Docker container provisioning

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "nginx_server"
+  image = docker_image.nginx.image_id
+  ports {
+    internal = 80
+    external = 8081
+  }
+}
+
